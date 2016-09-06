Pomax/react-onclickoutside

commit 8870fdf96edfecb2ddcfb0e899fd029e3976df82

Add support for config, allow users to specify onClickOutside handler (#114)
* add support for custom clickOutsideHandler, using a HOC options object

index.js
@@ -33,7 +33,7 @@
    * Generate the event handler that checks whether a clicked DOM node
    * is inside of, or lives outside of, our Component's node tree.
    */
-  var generateOutsideCheck = function(componentNode, componentInstance, ignoreClass, preventDefault, stopPropagation) {
+  var generateOutsideCheck = function(componentNode, componentInstance, eventHandler, ignoreClass, preventDefault, stopPropagation) {
     return function(evt) {
       if (preventDefault) {
         evt.preventDefault();
@@ -56,7 +56,7 @@
       // If element is in a detached DOM, consider it "not clicked
       // outside", as it cannot be known whether it was outside.
       if(current !== document) return;
-      componentInstance.handleClickOutside.bind(componentInstance, evt)();
+      eventHandler(evt);
     }
   };
 
@@ -69,7 +69,7 @@
   function setupHOC(root, React, ReactDOM) {
 
     // The actual Component-wrapping HOC:
-    return function(Component) {
+    return function(Component, config) {
       var wrapComponentWithOnClickOutsideHandling = React.createClass({
         statics: {
           /**
@@ -99,14 +99,24 @@
          */
         componentDidMount: function() {
           var instance = this.getInstance();
+          var clickOutsideHandler;
 
-          if(typeof instance.handleClickOutside !== "function") {
-            throw new Error("Component lacks a handleClickOutside(event) function for processing outside click events.");
+          if(config && typeof config.handleClickOutside === "function") {
+            clickOutsideHandler = config.handleClickOutside(instance);
+            if(typeof clickOutsideHandler !== "function") {
+              throw new Error("Component lacks a function for processing outside click events specified by the handleClickOutside config option.");
+            }
+          } else {
+              if(typeof instance.handleClickOutside !== "function") {
+                throw new Error("Component lacks a handleClickOutside(event) function for processing outside click events.");
+            }
+            clickOutsideHandler = instance.handleClickOutside;
           }
 
           var fn = this.__outsideClickHandler = generateOutsideCheck(
             ReactDOM.findDOMNode(instance),
             instance,
+            clickOutsideHandler,
             this.props.outsideClickIgnoreClass || IGNORE_CLASS,
             this.props.preventDefault || false,
             this.props.stopPropagation || false

test/test.js
@@ -56,4 +56,62 @@ describe('onclickoutside hoc', function() {
       assert(e, "component was not wrapped");
     }
   });
+
+
+  it('should call the specified handler when clicking the document', function() {
+    var CustomComponent = React.createClass({
+      getInitialState: function() {
+        return {
+          clickOutsideHandled: false
+        };
+      },
+
+      myOnClickHandler: function(event) {
+        if (event === undefined) {
+            throw new Error("event cannot be undefined");
+        }
+
+        this.setState({
+          clickOutsideHandled: true
+        });
+      },
+
+      render: function() {
+        return React.createElement('div');
+      }
+    });
+
+    var WrappedWithCustomHandler = wrapComponent(CustomComponent, {
+      handleClickOutside: function (instance) {
+        return instance.myOnClickHandler;
+      }
+    });
+
+    var element = React.createElement(WrappedWithCustomHandler);
+    assert(element, "element can be created");
+    var component = TestUtils.renderIntoDocument(element);
+    assert(component, "component renders correctly");
+    document.dispatchEvent(new Event('mousedown'));
+    var instance = component.getInstance();
+    assert(instance.state.clickOutsideHandled, "clickOutsideHandled got flipped");
+  });
+
+  it('should throw an error when a custom handler is specified, but the component does not implement it', function() {
+    var BadComponent = React.createClass({
+      render: function() {
+        return React.createElement('div');
+      }
+    });
+
+    try {
+      var bad = wrapComponent(BadComponent, {
+        handleClickOutside: function (instance) {
+          return instance.nonExistentMethod;
+        }
+      });
+      assert(false, "component was wrapped, despite not implementing the custom handler");
+    } catch (e) {
+      assert(e, "component was not wrapped");
+    }
+  });
 });

commit f6b7476f60efdbd15dc67cddca061d54fd33c671

Fixes regression of #88
This fixes a regression for #87/#98 introduced by #114.

index.js
@@ -108,12 +108,12 @@
               throw new Error('Component lacks a function for processing outside click events specified by the handleClickOutside config option.');
             }
           } else if(typeof instance.handleClickOutside === 'function') {
-            clickOutsideHandler = instance.handleClickOutside;
+            clickOutsideHandler = instance.handleClickOutside.bind(instance);
           } else if(typeof instance.props.handleClickOutside === 'function') {
-            clickOutsideHandler = instance.props.handleClickOutside;
+            clickOutsideHandler = instance.props.handleClickOutside.bind(instance);
           } else {
             throw new Error('Component lacks a handleClickOutside(event) function for processing outside click events.');
-          }             
+          }
 
           var fn = this.__outsideClickHandler = generateOutsideCheck(
             ReactDOM.findDOMNode(instance),