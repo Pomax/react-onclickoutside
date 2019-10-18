39b90249ddd4602a00e323c77d171b9ca6106bf1
Matt Thompson <whatknight@gmail.com>
2019-10-17 20:16:53 -0400

    fix broken minify because of unsupported option "warnings"

--- rollup.config.js
+++ rollup.config.js
@@ -39,8 +39,7 @@ const prodUmdConfig = mergeAll([
         compress: {
           pure_getters: true,
           unsafe: true,
-          unsafe_comps: true,
-          warnings: false,
+          unsafe_comps: true          
         },
       })
     ),
